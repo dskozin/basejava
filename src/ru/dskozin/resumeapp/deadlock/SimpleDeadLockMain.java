7ff736f1806ecbb8c5c823c987eabf7001d46936
Dmitriy Kozin <dskozin@mail.ru>
2018-01-14 16:20:20 +0300

    HW11 simple deadlock

---
+++ src/ru/dskozin/resumeapp/deadlock/SimpleDeadLockMain.java
@@ -0,0 +1,44 @@
+package ru.dskozin.resumeapp.deadlock;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleDeadLockMain {
+
+    //будет счетчик попыток блокировки
+    private volatile static int COUNTER = 0;
+
+    public static void main(String[] args) {
+
+        //создаем ресурсы для взаимной блокировки
+        Object resource1 = new Object();
+        Object resource2 = new Object();
+
+        //объявляем анонимные потоки. Один блокирует сначала ресурс1 потом ресурс2, второй наоборот
+        //когда заблокируется перестанет выводится надпись и счетчик
+        new Thread(){
+            @Override
+            public void run() {
+                while (true) {
+                    synchronized (resource1) {
+                        synchronized (resource2) {
+                            System.out.println("Blocking trying count:" + ++COUNTER);
+                        }
+                    }
+                }
+            }
+        }.start();
+
+        new Thread(){
+            @Override
+            public void run() {
+                while (true) {
+                    synchronized (resource2) {
+                        synchronized (resource1) {
+                            System.out.println("Blocking trying count:" + ++COUNTER);
+                        }
+                    }
+                }
+            }
+        }.start();
+    }
+}
